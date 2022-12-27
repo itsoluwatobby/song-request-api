1be012d38878a2c069e94d90ef53b59fea1d1101
itsoluwatobby <oluwasamuel050@gmail.com>
2022-12-27 05:22:03 +0100

    ready to deploy

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,8 @@
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://graciereacts.onrender.com']
 
 const corsOptions = {
    origin: (origin, callback) => {
-      allowedOrigins.includes(origin) || !origin ? callback(null, true) : callback(null, new Error('NOT ALLOWED BR CORS'))
+      allowedOrigins.includes(origin) ? callback(null, true) : callback(null, new Error('NOT ALLOWED BY CORS'))
    },
    credentials: true,
    optionsSuccessStatus: 200
